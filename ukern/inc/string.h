2b74eb195e0613807770f669d7682cb47ae198b0
Wang Lu <cnwanglu@icloud.com>
2023-06-04 18:35:59 +0800

    feat: add kernel lib <string.h>

---
+++ ukern/inc/string.h
@@ -0,0 +1,15 @@
+#pragma once
+#include <types.h>
+
+void memset(void *base, char ch, int size);
+void *memcpy(void *dest, const void *src, size_t n);
+int memcmp(const void *s1, const void *s2, size_t n);
+void *memmove(void *dest, const void *src, size_t n);
+void *memchr(const void *s, int c, size_t n);
+int strcmp(const char *s1, const char *s2);
+int strncmp(const char *s1, const char *s2, size_t n);
+char *strcpy(char *des, const char *src);
+char *strncpy(char *des, const char *src, int len);
+size_t strlen(const char *s);
+char *strchr(const char *src, char ch);
+char *strstr(const char *haystack, const char *needle);
