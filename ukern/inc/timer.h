d714a4119a3bb04771005c88c5385b9510cea11f
Wang Lu <cnwanglu@icloud.com>
2023-06-09 17:39:16 +0800

    feat(timer): 补充相关函数

--- ukern/inc/timer.h
+++ ukern/inc/timer.h
@@ -6,18 +6,19 @@
 typedef void (*timer_func_t)(unsigned long);
 
 struct timer {
-	/* int cpu; */
-	/* int stop; */
-	uint64_t expires;
+  /* int cpu; */
+  /* int stop; */
+  uint64_t expires;
   uint64_t timeout;
-	unsigned long data;
-	timer_func_t hook;
-	/* struct list_head entry; */
-	/* struct raw_timer *raw_timer; */
+  unsigned long data;
+  timer_func_t hook;
+  /* struct list_head entry; */
+  /* struct raw_timer *raw_timer; */
 };
 
 
 void timer_setup_and_start(struct timer *timer, u64 ns);
 void timer_setup(struct timer *timer, u64 ns);
 void timer_start(struct timer *timer);
-void timer_init(void);
+int timer_stop(struct timer *timer);
+void timer_init(struct timer *timer, timer_func_t fn, unsigned long data);
--- ukern/src/timer.c
+++ ukern/src/timer.c
@@ -1,5 +1,6 @@
 
 #include <arm64_timer.h>
+#include <preempt.h>
 #include <timer.h>
 #include <time.h>
 
@@ -9,14 +10,12 @@ void timer_start(struct timer *timer)
 
   arch_timer_setddl(timer->expires);
   arch_timer_enable();
-  
 }
 
 void timer_setup(struct timer *timer, u64 ns)
 {
-	timer->timeout = ns;
-  if (timer->timeout < TIMER_PRECISION)
-		timer->timeout = TIMER_PRECISION;
+  timer->timeout = ns;
+  if (timer->timeout < TIMER_PRECISION) timer->timeout = TIMER_PRECISION;
 }
 
 void timer_setup_and_start(struct timer *timer, u64 ns)
@@ -24,8 +23,45 @@ void timer_setup_and_start(struct timer *timer, u64 ns)
   timer->timeout = ns;
   timer_start(timer);
 }
-  
-void timer_init(void)
+
+int timer_stop(struct timer *timer)
 {
-  arch_timer_init();
+  // struct raw_timer *timers = timer->raw_timer;
+  // unsigned long flags;
+
+  // if (timer->cpu == -1)
+  // 	return 0;
+
+  // timer->stop = 1;
+  // ASSERT(timer->raw_timer != NULL);
+  // spin_lock_irqsave(&timers->lock, flags);
+  // /*
+  //  * wait the timer finish the action if already
+  //  * timedout.
+  //  */
+  // while (timers->running_timer == timer)
+  // 	cpu_relax();
+
+  // detach_timer(timers, timer);
+  // timer->cpu = -1;
+  // timer->expires = 0;
+  // spin_unlock_irqrestore(&timers->lock, flags);
+
+  return 0;
+}
+
+void timer_init(struct timer *timer, timer_func_t fn, unsigned long data)
+{
+  // arch_timer_init();
+
+  preempt_disable();
+
+  // timer->cpu = -1;
+  // timer->entry.next = NULL;
+  timer->expires = 0;
+  timer->timeout = 0;
+  timer->hook = fn;
+  timer->data = data;
+  // timer->raw_timer = NULL;
+  preempt_enable();
 }
