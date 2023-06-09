ab7abec54e3ebad9b8a6f51565fae654d76953aa
Wang Lu <cnwanglu@icloud.com>
2023-06-09 17:30:19 +0800

    feat(lib): add strncpy()

--- ukern/src/string.c
+++ ukern/src/string.c
@@ -1,1 +1,16 @@
 #include <string.h>
+
+char *strncpy(char *des, const char *src, int len)
+{
+	char *tmp = des;
+	int i;
+
+	if (des == NULL || src == NULL)
+		return NULL;
+
+	for (i = 0; i < len; i++) {
+		des[i] = src[i];
+	}
+
+	return tmp;
+}
